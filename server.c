15af49f9fe8681b7f5623c612a5b2b2ac9e6b734
Sandro Rybarik <sandro.rybarik@gmail.com>
2018-07-12 17:41:05 +0200

    client in C

--- server.c
+++ server.c
@@ -15,43 +15,43 @@
 #define MAXBUFLEN 1024
 
 int main () {
-    // man ip 7 | man 
-    // create socket
-    int udp_socket = socket(AF_INET, SOCK_DGRAM, 0);
-    int cfd;
-
-    if (udp_socket == -1) {
+    // create socket - man ip 7
+    int udp_socket;
+    if ((udp_socket = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
         handle_error("socket");
     }
 
     
-    struct sockaddr_in my_addr;
-
-    struct in_addr inaddr;
-    inaddr.s_addr = htonl(INADDR_LOOPBACK);
-
-    my_addr.sin_family = AF_INET;
-    my_addr.sin_port = htons(5000);
-    my_addr.sin_addr = inaddr;
+    struct sockaddr_in addr_listen_on;
+    addr_listen_on.sin_family = AF_INET; // AF_INET is an address family that is used to designate the type of addresses that your socket can communicate with (in this case, Internet Protocol v4 addresses)
+    addr_listen_on.sin_port = htons(5000);
     
-    // assigning a name to a socket is called binding
-    int b = bind(udp_socket, (struct sockaddr *)&my_addr, sizeof(struct sockaddr_in));
-
-    if (b == -1) {
+    struct in_addr inaddr;
+    inaddr.s_addr = htonl(INADDR_LOOPBACK); // the struct s_addr is a 4-byte number that represents one digit in an IP address per byte
+    
+    addr_listen_on.sin_addr = inaddr;
+    
+    // assigning a name to a socket is called binding & error check
+    if (bind(udp_socket, (struct sockaddr *)&addr_listen_on, sizeof(struct sockaddr_in))) {
         handle_error("bind");
     }
 
-    int numbytes = -1;
+    int numbytes = -1; // default value for failure; othertimes will hold number of incoming bytes in buf
     int addr_len = 0;
-    struct sockaddr_in their_addr;
-    char buf[MAXBUFLEN];
+    struct sockaddr_in addr_incoming_from_outside; // hold their address
+    char buf[MAXBUFLEN]; // space for holding incoming data in memory
 
-    printf("Server is running, waiting for connection\n");
+    printf("Server is running, waiting for connections\n");
 
     while(1) {
         addr_len = sizeof(struct sockaddr);
+
+        /**
+         * recvfrom() places the received message into the buffer buf.  The caller must specify the size of the buffer in len.
+         * If the caller is not interested in the source address, src_addr and addrlen should be specified as NULL.
+        */
         if ((numbytes = recvfrom(udp_socket, buf, MAXBUFLEN-1 , 0,
-            (struct sockaddr *)&their_addr, &addr_len)) == -1) {
+            (struct sockaddr *)&addr_incoming_from_outside, &addr_len)) == -1) {
             handle_error("recvfrom");
         }
 
