c9aa6e8398e15c35786dd855d96e2ba2643c3708
Patrik Siget <patrik.siget@gmail.com>
2018-07-18 16:47:56 +0200

    added database, rendering data to website

--- README.md
+++ README.md
@@ -12,5 +12,8 @@
 ## webserver
 1. install `nodejs-legacy`
 2. install `npm`
-3. go to `websrv/`
-4. start by `node index.js`
+3. install `mariadb` client and server
+ - set up mysql [mysql getting started](https://dev.mysql.com/doc/mysql-getting-started/en/)
+ - delete `.example` from `/websrv/database/db.js.example` and configure
+4. go to `websrv/`
+5. start by `node index.js`
--- websrv/app.js
+++ websrv/app.js
@@ -1,7 +1,11 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+const express = require('express');
 const app = express();
-const data = require(__dirname + '/dummy.json');
+const bodyParser = require('body-parser');
+var db = require('./database/db');
+const fs = require('fs');
+const readline = require('readline');
+const async = require('async');
+
 
 //port setting
 var port = 3000;
@@ -17,16 +21,46 @@ app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 
+//various app data ** TEMPORARY, this data will be fetched from db **
+var data = require('./dummy.json');
+
+//routes
 app.get('/', (req, res) => {
-    res.render('index', data);
-})
+    db.query(`SELECT * FROM my_data`, (err, rows) => {
+        console.log(rows);
+        res.render('main', {datas: rows});
+    });
+});
 
-app.get('/logfile', (req, res) => {
-    res.render('data');
-})
+//infinite async loop which reads from log file and store data in db
+const delay = 1000;
+
+async.forever(
+(next) => {
+
+    fs.readFile('logfile', function(err, datas) {
+        if(err) throw err;
+        var array = datas.toString().split("\n");
+        for(i in array) {
+            if (array[i]){
+                db.query(`INSERT INTO my_data VALUES (null, "${array[i]}", null)`);
+                console.log(array[i]);
+            }
+        }
+        
+        fs.writeFile('./logfile', '', function(){});
+        
+    });
+
+    setTimeout(next, delay);
+}, 
+
+(err) => {
+    console.log(err);
+});
 
 
+//start server
 app.listen(port, () => {
     console.log(`Server listening on ${port}`);
-    // console.log();
 })
