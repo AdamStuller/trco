cd56767e18127d12eb5a0c75790f1a9064e1db64
Patrik Siget <patrik.siget@gmail.com>
2018-08-01 23:25:51 +0200

    added comments, todos, ...

--- src/server.c
+++ src/server.c
@@ -4,10 +4,10 @@
 int main () {
 
     struct CONFIG config;
-    int e, sockfd;
-    struct sockaddr_in clientaddr;
+    int e, sockfd;                                              //e - error checker(not implemented), sockfd - socket file descriptor
+    struct sockaddr_in clientaddr;                              
     char incomeMsg[MAXBUFLEN], outcomeMsg[MAXBUFLEN];
-    char * output;
+    char * output;                                              //moze sa dynamicky menit v processData()? // not implemented
 
     //load and verify config file
     if ( (e = load_config("config/server.cfg", &config) ) != 0){
@@ -15,17 +15,30 @@ int main () {
     }
     print_CONFIG(config);
     
+    //creates socket file descriptor and realizes bind
     run_srv(config, &sockfd);
 
     for(;;){
         
+        /*
+            clientaddr sa vzdy nastavi na adresu clienta v rcv()
+        */
+
+
+        //recieve from client, set clientaddr
         rcv(sockfd, incomeMsg, &clientaddr);
        
-        //processData(incomeMsg, output, outcomeMsg);
+        /*
+            incomeMsg je je raw sprava od clienta,
+            teraz je uz len na uvazeni, ci chceme v main mat nejaky data processing alebo to pojde rovno
+            do processData(), ktora do output vypise chceny text z packetu a outcomeMsg ktory sa posle na client ako succes/fail/todo/request...
+        */
+
+        //processData(incomeMsg, output, outcomeMsg);           //not implemented
 
         printf("%s", incomeMsg);
 
-        //snd(sockfd, outcomeMsg, clientaddr);
+        //snd(sockfd, outcomeMsg, clientaddr);                  //not implemented
 
     }
 
