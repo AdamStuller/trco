824682ba528a1b86e85555d54e64e283b8822c97
Sandro Rybárik <sandro.rybarik@gmail.com>
2018-08-07 14:30:36 +0200

    logs incoming raw messages to file

--- src/server.c
+++ src/server.c
@@ -20,23 +20,37 @@ int main (int argc, char *argv[]) {
     //creates socket file descriptor and realizes bind
     run(config, &sockfd, SERVER_SIDE);
     
+    FILE * log_data = fopen(config.DAT_FILE, "a+"); 
+    if (log_data == NULL) {
+        perror("Cannot open Data file.\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    printf("DATA FILE %s OPENED.\n", config.DAT_FILE);
+
     for(;;){
 
         //recieve from client, set clientaddr
         if (rcv(sockfd, incomeMsg, &clientaddr) != 0) {
             // error case
+            perror("rcv Failed\n");
+        } else {
+            fprintf(log_data, "%s\n", incomeMsg);
+            fflush(log_data);
         }
 
         printf("%s\n", incomeMsg);
     
         if (snd(sockfd, "OKs\0", &clientaddr) != 0) {
             // error case
+        } else {
+            ;
         }
     }
 
 
     free(incomeMsg);
     free(outcomeMsg);
-    
+    fclose(log_data);
     return 0;
 }
